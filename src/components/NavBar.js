0ecf7d74f8b6fb0683e1a703a00452907e9671ca
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-12-27 14:19:48 -0600

    update

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import {useRef}from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+
+const NavBar = () => {
+    
+    const navRef = useRef();
+    const showNavBar =()=> {
+        navRef.current.classList.toggle('responsive_var')
+    }
+
+  return (
+    <header>
+    <nav ref ={navRef}>
+      <a href='/'>Home</a>
+      <a href='/portafolio'>Portafolio</a>
+      <a href='/about'>About</a>
+      <a href='/contact'>Contact</a>
+      <button onClick={showNavBar} className=' nav-btn nav-close-btn '>
+        <FaTimes/>
+      </button>
+      
+    </nav>
+    <button onClick={showNavBar} className='nav-btn'> 
+        <FaBars/>
+      </button>
+    </header>
+  );
+}
+
+export default NavBar;
