05b1471dfa51c0469f9c6ed21e92955ac9dd94af
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-12-27 20:54:48 -0600

    update-added-routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import {Routes,Route} from 'react-router-dom'
 import './App.css';
-
 import NavBar from './components/NavBar';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <>
-   <NavBar/>
+    <NavBar/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+     </Routes>
+   
     </>
   );
 }
