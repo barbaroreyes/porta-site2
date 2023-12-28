944adf36bcb17a34d72bfb4c0c4fbab3d8ed18ca
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-12-28 11:47:25 -0600

    update-build

--- src/Pages/Portafolio.js
+++ src/Pages/Portafolio.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Portafolio = () => {
   return (
-    <div>
-      Portafolio
+    <div className="portafolio-container">
+      <h1>Dev</h1>
+      <h1>UX/UI Design</h1>
     </div>
   );
 }
