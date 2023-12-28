8b143676dae3e4c7cdfe659706eebb9ec42518d4
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-12-28 10:56:08 -0600

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       
        <div className="picture-container1">
       <img src=" https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' />
-      
+        
       </div>
       <div className="text-container">
      
