96e954d691c04565df68a655505561a69af7e43d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-12-28 00:33:39 -0600

    update-home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="home_container">
-      
-    </div>
+    <div className="container">
+       <div className="picture-container1">
+      <img src=" https://avatars.githubusercontent.com/u/48423441?v=4 " alt='' />
+  
+      </div>
+      <div className="text-container">
+      <h3>Barbaro Reyes</h3>
+      <h3>Dev & UX/UI</h3>
+      </div>
+     
+   
+   </div>
   );
 }
 
